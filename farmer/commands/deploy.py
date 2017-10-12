4d685ad94f1a45e9ad805b886443524433d63145
Paweł Adamczak <pawel.ad@gmail.com>
2017-10-12 08:28:23 -0400

    Added support for 'branch' argument to deploy command

--- farmer/commands/deploy.py
+++ farmer/commands/deploy.py
@@ -11,12 +11,13 @@ from farmer.config import load_config
 @click.command()
 @click.argument('app')
 @click.argument('environment')
+@click.option('-b', '--branch', help="Name of the branch that should be deployed.")
 @click.option(
     '--open/--no-open', 'open_deploy',
     help='Open deploy page in portal automatically.',
     default=True, show_default=True
 )
-def cli(app, environment, open_deploy):
+def cli(app, environment, branch, open_deploy):
     """
     Deploy an application to an environment.
     """
@@ -26,7 +27,12 @@ def cli(app, environment, open_deploy):
         application_list = client.get('applications')['results']
         selected_application = next(application for application in application_list if application['name'] == app)
         assert environment in selected_application['environments'], 'Invalid environment specified.'
-        response = client.post('applications/{application_id}/builds'.format(**selected_application), data={'environment': environment})
+
+        data = {
+            'environment': environment,
+            'branch': branch,
+        }
+        response = client.post('applications/{application_id}/builds'.format(**selected_application), data=data)
         build_id = response['id']
         deploy_url = client.url_for('builds', 'deploys', build_id)
     except AssertionError as exc:
